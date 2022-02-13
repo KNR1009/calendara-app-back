c152741e387fe03396ab5c60869ea1c15ca78f2e
伊東和成 <itokazunari5375@icloud.com>
2022-02-13 09:42:08 +0900

    削除用のapiを作成

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class EventsController < ApplicationController
-      before_action :event_params, only: [:create]
+      before_action :event_params, only: [:create, :update]
       def index
         render json: Event.all
       end
@@ -12,8 +12,27 @@ module Api
         else
           render json: {message: event.errors.messages}
         end
-      
       end
+
+      def update
+        event = Event.find(params[:id])
+        if event.update(event_params)
+          render json: {messages: "更新しました"}
+        else
+          render json: {message: event.errors.messages}
+        end
+      end
+
+      def destroy
+        event = Event.find(params[:id])
+        if event.delete
+          render json: {message: "削除しました"}
+        else
+          render json: {messages: "削除に失敗しました"}
+        end
+      end
+
+
       def show
         event = Event.find(params[:id])
         render json: event
