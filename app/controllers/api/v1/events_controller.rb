4451c94051f16cfff5f179a2d3b9cf489d0b29bd
伊東和成 <itokazunari5375@icloud.com>
2022-02-13 09:17:57 +0900

    新規作成のapi

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,8 +1,28 @@
 module Api
   module V1
     class EventsController < ApplicationController
+      before_action :event_params, only: [:create]
       def index
         render json: Event.all
+      end
+      def create
+        event = Event.new(event_params)
+        if event.save
+          render json: event
+        else
+          render json: {message: "登録に失敗しました"}
+        end
+      
+      end
+      def show
+        event = Event.find(params[:id])
+        render json: event
+      end
+
+      private
+      
+      def event_params
+        params.permit(:id, :name, :start, :end, :timed, :description, :color)
       end
     end
   end
